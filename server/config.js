2b9a795fbc59437421493a557ce51df684eda446
dulx <dulx96@gmail.com>
2018-04-24 13:39:22 +0700

    1st

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTHO_DOMAIN: '[dulx96.au.auth0.com]',
+  AUTH0_API_AUDIENCE: '[http://localhost:8083/api/]',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://root:123456@ds247759.mlab.com:47759/?authSource=mean-rsvp'
+}
